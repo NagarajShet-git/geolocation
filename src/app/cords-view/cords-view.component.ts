3daa10881c7ba42268acb889c6dcd7510d9bd417
NagarajShet-git <54940687+NagarajShet-git@users.noreply.github.com>
2019-09-06 14:13:12 +0530

    Add files via upload

--- src/app/cords-view/cords-view.component.ts
+++ src/app/cords-view/cords-view.component.ts
@@ -10,6 +10,7 @@ import { Geometry } from "../geo-search/geometry";
 export class CordsViewComponent implements OnInit, OnChanges {
   @Input() search_text;
   show_cords: boolean = false;
+  error_city: boolean = false;
   error: {};
 
   cords = {
@@ -32,15 +33,25 @@ export class CordsViewComponent implements OnInit, OnChanges {
   getGeoCords() {
     this.geoService.getLocation(this.search_text).subscribe(
       (data: Geometry) => {
-        this.cords.southwest.lat =
-          data.results[0].geometry.viewport.southwest.lat;
-        this.cords.southwest.lng =
-          data.results[0].geometry.viewport.southwest.lng;
-        this.cords.northeast.lat =
-          data.results[0].geometry.viewport.northeast.lat;
-        this.cords.northeast.lng =
-          data.results[0].geometry.viewport.northeast.lng;
-        this.show_cords = true;
+        if (data.results[0]) {
+          this.cords.southwest.lat =
+            data.results[0].geometry.viewport.southwest.lat;
+          this.cords.southwest.lng =
+            data.results[0].geometry.viewport.southwest.lng;
+          this.cords.northeast.lat =
+            data.results[0].geometry.viewport.northeast.lat;
+          this.cords.northeast.lng =
+            data.results[0].geometry.viewport.northeast.lng;
+        }
+        if (
+          this.cords.northeast.lat !== null ||
+          this.cords.southwest.lng !== null
+        ) {
+          this.show_cords = true;
+          this.error_city = false;
+        } else {
+          this.error_city = true;
+        }
       },
       error => {
         this.error = error;
