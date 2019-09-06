8887b9745b45155b7a846129a0f2fd8c29272330
NagarajShet-git <54940687+NagarajShet-git@users.noreply.github.com>
2019-09-06 14:00:44 +0530

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
-import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 
 import { GeoSearchComponent } from "./geo-search/geo-search.component";
@@ -10,7 +9,7 @@ import { CordsViewComponent } from "./cords-view/cords-view.component";
 
 @NgModule({
   declarations: [AppComponent, GeoSearchComponent, CordsViewComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
